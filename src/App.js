b81d6d35d8c7e9740990dbef8f0c212081c3f9e8
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-01-07 20:24:14 -0500

    NASA Data Fetched

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
+function useFetchNasaData()
+{
+
+  const [nasaData, setNasaData] = useState('');
+
+  useEffect(() => {
+
+    const fetchNasaData = () => {
+      // This axios GET request will return a single image
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        // Which we then set to state
+        .then(res => setNasaData(res.data))
+        // Always include error handling
+        .catch(err => console.log(err));
+    }
+
+    fetchNasaData();
+  }, []);
+
+  return nasaData;
+}
+
+
 function App() {
+
+  const nasaData = useFetchNasaData();
+
   return (
-    <div className="App">
+    <div className="App">{console.log(nasaData)}
+      <h1>{nasaData.title}</h1>
+      <h4>{nasaData.date}</h4>
+      <img src={nasaData.hdurl} height="400" width="1200"></img>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        {nasaData.explanation}
       </p>
     </div>
   );
